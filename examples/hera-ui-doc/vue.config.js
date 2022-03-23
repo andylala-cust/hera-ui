efb0b61fcbd9cdb11edc5c6cfdf0a1044c5d2db8
andylala <857511368@qq.com>
2022-03-24 00:18:36 +0800

    feat 修改相关配置

--- examples/hera-ui-doc/vue.config.js
+++ examples/hera-ui-doc/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     configureWebpack: {
         resolve: {
             alias: {
-                main: "../../"
+                main: "../../../"
             }
         }
     }
