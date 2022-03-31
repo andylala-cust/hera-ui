3c319e2ada3aff5ac4c2356e803dab419c375aaa
andylala <857511368@qq.com>
2022-04-01 00:07:49 +0800

    feat doc demo

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Welcome from '@/components/pages/welCome'
+import doc from '@/components/pages/doc'
+import panelPage from '@/components/pages/panelPage'
+import iconPage from '@/components/pages/iconPage'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Welcome',
+            component: Welcome
+        },
+        {
+            path: '/doc',
+            name: 'doc',
+            component: doc,
+            redirect: '/doc/icon',
+            children: [
+                {
+                    path: '/doc/icon',
+                    name: 'iconPage',
+                    component: iconPage
+                },
+                {
+                    path: '/doc/panel',
+                    name: 'panelPage',
+                    component: panelPage
+                }
+            ]
+        }
+    ]
+})
